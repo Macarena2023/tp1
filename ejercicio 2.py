b5e0fce245f076cef4d50d30db90eb5e04cefcea
Macarena2023 <149072484+Macarena2023@users.noreply.github.com>
2024-04-17 10:08:05 -0300

    Create ejercicio 2.py

---
+++ ejercicio 2.py
@@ -0,0 +1,25 @@
+a = 10
+b = 5
+
+suma = a + b
+resta = a - b
+multiplicacion = a * b
+division = a / b
+modulo = a % b
+exponente = a ** b
+
+print("resta:", resta)
+print("suma:", suma)
+print("multiplicacion:", multiplicacion)
+print("division:", division)
+print("modulo:", modulo)
+print("exponente:", exponente)
+
+comparacion = suma > resta
+print("comparacion:", suma > resta)
+
+comparacion = multiplicacion > division
+print("comparacion:", multiplicacion > division)
+
+comparacion = modulo > exponente
+print("comparacion:", modulo > exponente)
