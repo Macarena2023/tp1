9bcf456425fa279c4e8d82e441ea3acd83cc24c8
Macarena2023 <149072484+Macarena2023@users.noreply.github.com>
2024-04-17 10:16:18 -0300

    Create ejercicio 5.py

---
+++ ejercicio 5.py
@@ -0,0 +1,8 @@
+datos_personales = {"nombre": "macarena", "edad": 17, "ciudad": "cordoba"}
+
+print(datos_personales["nombre"])
+
+datos_personales["edad"]= 18
+datos_personales["sexo"]= "masculino"
+
+print("datos personales:", datos_personales)
