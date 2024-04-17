6340068fefbdd0ff556935bed4c11c3fc5f4ddb5
Macarena2023 <149072484+Macarena2023@users.noreply.github.com>
2024-04-17 10:06:43 -0300

    Create ejercicio 1.py

---
+++ ejercicio 1.py
@@ -0,0 +1,10 @@
+Numero_entero = 10
+Numero_flotante = 5.12
+Cadena = "hola equisde"
+Es_verdadero = True
+
+
+print("Numero entero:", Numero_entero)
+print("Numero flotante:", Numero_flotante)
+print("Cadena", Cadena)
+print("Es verdadero", Es_verdadero)
